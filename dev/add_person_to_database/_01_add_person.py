f0d416e7a1136caa054f5e6eb293459b4b2689b5
vesran <tran.yves@gmail.com>
2020-08-10 11:54:42 +0200

    extrated face from videos possible

--- dev/add_person_to_database/_01_add_person.py
+++ dev/add_person_to_database/_01_add_person.py
@@ -6,7 +6,9 @@ The main function should create a new folder containing images of the person's f
 
 '''
 
+from datetime import date
 import os
+import re
 from glob import glob
 import cv2
 import yaml
@@ -35,7 +37,7 @@ def load_models(verbose=False):
     return detector, embedder
 
 
-def extract_faces_from_stream(name, verbose=False):
+def extract_faces_from_stream(name, verbose=False, delay=0.5):
     # Create the folder of not exists
     path = os.path.join(params['dataset'], name)
     if not os.path.exists(path):
@@ -99,11 +101,12 @@ def extract_faces_from_stream(name, verbose=False):
                 continue
 
             color = (0, 0, 255)
-            if nb_photo_taken < params['nb_photos'] and time.time() - start_time > 0.5:
+            if nb_photo_taken < params['nb_photos'] and time.time() - start_time > delay:
                 color = (0, 255, 0)
                 start_time = time.time()
                 face = frame[startY:endY, startX:endX]
-                filename = os.path.join(path, f"{str(nb_photo_taken).zfill(3)}.jpg")
+                filename = os.path.join(path,
+                                        f"{str(nb_photo_taken).zfill(3)}_{name}_{re.sub('-', '', str(date.today()))}.jpg")
                 cv2.imwrite(filename, face)
                 print(f'[INFO] Writing photo {filename} to disk') if verbose else 0
                 nb_photo_taken += 1
@@ -133,12 +136,83 @@ def extract_faces_from_stream(name, verbose=False):
     return path
 
 
-def extract_faces_from_video(videopath):
-    pass
+def extract_faces_from_video(name, video_path, verbose=False, delay=0.5):
+    # Create the folder of not exists
+    path = os.path.join(params['dataset'], name)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    else:
+        # Clean directory if exists
+        files = glob(os.path.join(path, '*'))
+        for f in files:
+            print(f"Removing {f}")
+            os.remove(f)
+
+    nb_photo_taken = 0
+    cap = cv2.VideoCapture(video_path)
+    detector, embedder = load_models(verbose=verbose)
+    start_time = time.time()
+    video_resolution = params['video_resolution']
+    face_resolution = params['face_resolution']
+
+    while cap.isOpened():
+        _, frame = cap.read()
+        if frame is None:
+            break
+
+        frame = imutils.resize(frame, width=video_resolution[1])
+        (h, w) = frame.shape[:2]
+
+        # construct a blob from the image
+        imageBlob = cv2.dnn.blobFromImage(cv2.resize(frame, face_resolution), 1.0, face_resolution,
+                                          (104.0, 177.0, 123.0), swapRB=False, crop=False)
+        # apply OpenCV's deep learning-based face detector to localize
+        # faces in the input image
+        detector.setInput(imageBlob)
+        detections = detector.forward()
+
+        # loop over the detections
+        # extract the confidence (i.e., probability) associated with
+        # the prediction
+        i = np.argmax(detections[0, 0, :, 2])
+        confidence = detections[0, 0, i, 2]
+        # filter out weak detections
+        if confidence > params['confidence']:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the face
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            # extract the face ROI
+            face = frame[startY:endY, startX:endX]
+            (fH, fW) = face.shape[:2]
+            # ensure the face width and height are sufficiently large
+            if fW < 20 or fH < 20:
+                continue
+
+            color = (0, 0, 255)
+            if nb_photo_taken < params['nb_photos'] and time.time() - start_time > delay:
+                color = (0, 255, 0)
+                start_time = time.time()
+                face = frame[startY:endY, startX:endX]
+                filename = os.path.join(path,
+                                        f"{str(nb_photo_taken).zfill(3)}_{name}_{re.sub('-', '', str(date.today()))}.jpg")
+                cv2.imwrite(filename, face)
+                print(f'[INFO] Writing photo {filename} to disk') if verbose else 0
+                nb_photo_taken += 1
+
+            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    return path
 
 
 if __name__ == '__main__':
-    data_path = extract_faces_from_stream('moi', verbose=True)
+    data_path = extract_faces_from_video('Rachel', './images/rachel_alone.mp4', verbose=True, delay=1.0)
     assert len(glob(os.path.join(data_path, '*'))) == params['nb_photos']
 
 
