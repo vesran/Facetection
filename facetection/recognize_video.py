9f17315a535fd895ef82f79373e1e9d8ca70290a
vesran <tran.yves@gmail.com>
2020-09-01 14:28:57 +0200

    record video

--- facetection/recognize_video.py
+++ facetection/recognize_video.py
@@ -9,6 +9,10 @@ def recognize_from_video(video_path):
     cap = cv2.VideoCapture(video_path)
     video_resolution = params.video_resolution
 
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    out = cv2.VideoWriter('./images/friend_scene_output.mp4', cv2.VideoWriter_fourcc(*'MP4V'), 20, (frame_width, frame_height))
+
     while cap.isOpened():
         _, frame = cap.read()
         if frame is None or cv2.waitKey(1) & 0xFF == ord('q'):
@@ -24,10 +28,12 @@ def recognize_from_video(video_path):
 
         # Display frame
         cv2.imshow('frame', frame)
+        out.write(frame)
 
+    out.release()
     cap.release()
     cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    recognize_from_video('./images/friends_scene.mp4')
+    recognize_from_video('./images/local/friends_scene.mp4')
